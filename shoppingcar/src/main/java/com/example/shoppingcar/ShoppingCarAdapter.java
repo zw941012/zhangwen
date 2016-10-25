5efe13ab9b3da99b46505e6629619c8c18ac3223
zhangwen <798619806@qq.com>
2016-10-25 21:50:24 +0800

    第一次提交

---
+++ shoppingcar/src/main/java/com/example/shoppingcar/ShoppingCarAdapter.java
@@ -0,0 +1,31 @@
+package com.example.shoppingcar;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 张稳 on 2016/10/25.
+ */
+
+public class ShoppingCarAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
